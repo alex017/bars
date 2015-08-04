0d21cc290ae7ea2d0c240497dced64eed2d0dc60
Alexei Stepanov <stephanov.al@gmail.com>
2015-08-04 17:41:38 +0300

    added admin site

--- bars/ts/admin.py
+++ bars/ts/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 from .models import Theme, Question, Answer
 
+class AnswerAdmin(admin.TabularInline):
+    model = Answer
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question', {'fields' : ['name']}),
+        ('Theme', {'fields' : ['theme_name']}),
+    ]
+    inlines = [AnswerAdmin]
+    list_display = ('name', 'theme_name')
+    list_filter = ['theme_name']
+
 admin.site.register(Theme)
+admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Question)
-
-admin.site.register(Answer)
-# Register your models here.
